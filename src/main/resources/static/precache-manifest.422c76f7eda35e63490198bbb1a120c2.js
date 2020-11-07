538e07f3d8e3238f00e82dd095288e20affdef36
ElghaziMouad <elghazi.mouad1@gmail.com>
2020-11-07 15:56:10 +0000

    react

---
+++ src/main/resources/static/precache-manifest.422c76f7eda35e63490198bbb1a120c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a087d8be8b31f2d9cbed2f752eec0c8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "639255d239ff2b2768ed",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b8d589eb1aebf62f2fe2",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "639255d239ff2b2768ed",
+    "url": "/static/js/2.66e3aab6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.66e3aab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8d589eb1aebf62f2fe2",
+    "url": "/static/js/main.b11f12d0.chunk.js"
+  },
+  {
+    "revision": "79604cb468ac1f105456",
+    "url": "/static/js/runtime-main.85a1d62b.js"
+  }
+]);
